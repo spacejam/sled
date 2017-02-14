44dcac78a1ddfae9f29cdf4261cdde2d4ff094de
Tyler Neely <t@jujit.su>
2017-02-14 02:03:07 -0500

    iron out some bugs with lock-free io buffer

--- src/iobuf.rs
+++ src/iobuf.rs
@@ -8,33 +8,61 @@ const N_BUFS: usize = 8;
 
 struct IOBufs {
     log_offset: Arc<AtomicUsize>,
-    bufs: [Arc<UnsafeCell<[u8; MAX_BUF_SZ]>>; N_BUFS],
+    bufs: [Arc<UnsafeCell<Vec<u8>>>; N_BUFS],
     headers: [Arc<AtomicUsize>; N_BUFS],
     current_buf: Arc<AtomicUsize>,
     written_bufs: Arc<AtomicUsize>,
 }
 
+impl Clone for IOBufs {
+    fn clone(&self) -> IOBufs {
+        IOBufs {
+            log_offset: self.log_offset.clone(),
+            bufs: [self.bufs[0].clone(),
+                   self.bufs[1].clone(),
+                   self.bufs[2].clone(),
+                   self.bufs[3].clone(),
+                   self.bufs[4].clone(),
+                   self.bufs[5].clone(),
+                   self.bufs[6].clone(),
+                   self.bufs[7].clone()],
+            headers: [self.headers[0].clone(),
+                      self.headers[1].clone(),
+                      self.headers[2].clone(),
+                      self.headers[3].clone(),
+                      self.headers[4].clone(),
+                      self.headers[5].clone(),
+                      self.headers[6].clone(),
+                      self.headers[7].clone()],
+            current_buf: self.current_buf.clone(),
+            written_bufs: self.written_bufs.clone(),
+        }
+    }
+}
+
+unsafe impl Send for IOBufs {}
+unsafe impl Sync for IOBufs {}
+
+
 struct Reservation {
     offset: u32,
-    buf: Arc<UnsafeCell<[u8; MAX_BUF_SZ]>>,
+    buf: Arc<UnsafeCell<Vec<u8>>>,
     last_hv: u32,
     header: Arc<AtomicUsize>,
     current_buf: Arc<AtomicUsize>,
     written_bufs: Arc<AtomicUsize>,
 }
 
-unsafe impl Sync for IOBufs {}
-
 impl Default for IOBufs {
     fn default() -> IOBufs {
-        let mut bufs = [Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ])),
-                        Arc::new(UnsafeCell::new([0; MAX_BUF_SZ]))];
+        let mut bufs = [Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ])),
+                        Arc::new(UnsafeCell::new(vec![0; MAX_BUF_SZ]))];
         let headers = [Arc::new(AtomicUsize::new(0)),
                        Arc::new(AtomicUsize::new(0)),
                        Arc::new(AtomicUsize::new(0)),
@@ -56,7 +84,6 @@ impl Default for IOBufs {
 impl IOBufs {
     fn reserve(&self, len: u32) -> Reservation {
         loop {
-            println!("reserve loop");
             // load atomic progress counters
             let current_buf = self.current_buf.load(Ordering::SeqCst);
             let written = self.written_bufs.load(Ordering::SeqCst);
@@ -70,7 +97,7 @@ impl IOBufs {
 
             // load current header value
             let header = self.headers[idx].clone();
-            let hv = header.load(Ordering::SeqCst) as u32;
+            let mut hv = header.load(Ordering::SeqCst) as u32;
 
             // skip if already sealed
             if hv >> 31 == 1 {
@@ -82,25 +109,44 @@ impl IOBufs {
             // try to claim space, seal otherwise
             let offset = hv << 8 >> 8;
             assert!(len >> 24 == 0);
-            assert!(offset + len <= MAX_BUF_SZ as u32);
             if offset + len > MAX_BUF_SZ as u32 {
                 // attempt seal once, then start over
-                let new = hv | (1 << 31);
-                if header.compare_and_swap(hv as usize, new as usize, Ordering::SeqCst) ==
+                let sealed = hv | (1 << 31);
+                if header.compare_and_swap(hv as usize, sealed as usize, Ordering::SeqCst) ==
                    hv as usize {
+                    // TODO cleanup
                     // we succeeded in setting seal, so we get to bump cur
                     self.current_buf.fetch_add(1, Ordering::SeqCst);
+                    let n_writers = hv << 1 >> 25;
+                    if n_writers == 0 {
+                        // nobody else is going to flush this, so we need to
+                        let res = Reservation {
+                            offset: offset,
+                            buf: self.bufs[idx].clone(),
+                            last_hv: sealed,
+                            header: header,
+                            current_buf: self.current_buf.clone(),
+                            written_bufs: self.written_bufs.clone(),
+                        };
+                        res.write(vec![]);
+                    }
+                } else {
                 }
                 continue;
             } else {
                 // attempt to claim
-                let new = hv + len;
-                if header.compare_and_swap(hv as usize, new as usize, Ordering::SeqCst) !=
+                let writer_inc = 1 << 24;
+                let claimed = hv + len + writer_inc;
+                if header.compare_and_swap(hv as usize, claimed as usize, Ordering::SeqCst) !=
                    hv as usize {
                     // CAS failed, start over
                     continue;
                 }
+                hv = claimed;
             }
+
+            let n_writers = hv << 1 >> 25;
+            assert!(n_writers != 0);
 
             return Reservation {
                 offset: offset,
@@ -114,7 +160,6 @@ impl IOBufs {
     }
 
     pub fn write(&self, buf: Vec<u8>) {
-        println!("IOBufs write");
         let res = self.reserve(buf.len() as u32);
         res.write(buf);
     }
@@ -128,19 +173,18 @@ impl Reservation {
     fn commit_tx(&self) {}
 
     fn write(&self, buf: Vec<u8>) {
+        let range = (self.offset as usize..(self.offset as usize + buf.len()));
         unsafe {
-            let mut out_buf = self.buf.get();
-            (*out_buf)[self.offset as usize..].copy_from_slice(&*buf);
+            let mut out_buf = (*self.buf.get()).as_mut_slice();
+            (out_buf)[range].copy_from_slice(&*buf);
         }
 
         // decr writer count, retrying
         let mut hv = self.last_hv;
         loop {
             let n_writers = hv << 1 >> 25;
-            assert!(n_writers != 0);
             let sealed = hv >> 31 == 1;
-
-            if sealed && n_writers == 1 {
+            if sealed && n_writers == 0 {
                 // if sealed is set, and you are last decr,
                 // don't decr, that's writer's resp
                 self.slam_down_pipe();
@@ -151,6 +195,14 @@ impl Reservation {
             if self.header.compare_and_swap(hv as usize, hv2 as usize, Ordering::SeqCst) ==
                hv as usize {
                 // we succeeded, free to exit
+                // TODO cleanup
+                let n_writers = hv2 << 1 >> 25;
+                let sealed = hv2 >> 31 == 1;
+                if sealed && n_writers == 0 {
+                    // if sealed is set, and you are last decr,
+                    // don't decr, that's writer's resp
+                    self.slam_down_pipe();
+                }
                 return;
             } else {
                 // we failed, reload and retry
@@ -160,15 +212,11 @@ impl Reservation {
     }
 
     fn slam_down_pipe(&self) {
-        let mut out_buf = unsafe { self.buf.get() };
-
         // put the buf identified by idx on disk
         // TODO
 
-        // zero the buf
-        unsafe {
-            (*out_buf)[..].copy_from_slice(&[0u8; MAX_BUF_SZ]);
-        }
+        // reset the header, no need to zero the buf
+        self.header.store(0, Ordering::SeqCst);
 
         // bump self.written by 1
         self.written_bufs.fetch_add(1, Ordering::SeqCst);
@@ -177,10 +225,28 @@ impl Reservation {
 
 #[test]
 fn basic_functionality() {
-    let iobs = IOBufs::default();
-    for i in 1..5_000 {
-        let buf = vec![1; 1024];
-        iobs.write(buf);
-    }
-    println!("yo");
+    let iobs1 = Arc::new(IOBufs::default());
+    let iobs2 = iobs1.clone();
+    let iobs3 = iobs1.clone();
+    let t1 = thread::spawn(move || {
+        for i in 0..50_000 {
+            let buf = vec![1; 512];
+            iobs1.write(buf);
+        }
+    });
+    let t2 = thread::spawn(move || {
+        for i in 0..50_000 {
+            let buf = vec![2; 512];
+            iobs2.write(buf);
+        }
+    });
+    let t3 = thread::spawn(move || {
+        for i in 0..50_000 {
+            let buf = vec![3; 512];
+            iobs3.write(buf);
+        }
+    });
+    t1.join();
+    t2.join();
+    t3.join();
 }
