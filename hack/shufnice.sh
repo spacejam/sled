a709924150374c340aab0ec9bdfb79194a3191db
Tyler Neely <t@jujit.su>
2017-02-28 15:19:10 -0500

    fix up niceness shuffler

--- hack/shufnice.sh
+++ hack/shufnice.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-PID=`pgrep $1`
 while true; do
+  PID=`pgrep $1`
   TIDS=`ls /proc/$PID/task`
-  TID=`echo $TIDS | shuf -n1`
+  TID=`echo $TIDS |  tr " " "\n" | shuf -n1`
   NICE=$((`shuf -i 0-39 -n 1` - 20))
   echo "renicing $TID to $NICE"
   renice -n $NICE -p $TID
+  sleep 0.1
 done
