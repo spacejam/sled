f20cb087884ada6c86e36b1c41d6a848d3a76045
Tyler Neely <t@jujit.su>
2017-08-08 00:37:48 +0200

    fix bad conflict res

--- src/page/lru.rs
+++ src/page/lru.rs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 use std::collections::{BTreeMap, HashMap};
 use std::sync::RwLock;
 use std::sync::atomic::{AtomicIsize, AtomicUsize, Ordering};
@@ -74,36 +73,6 @@ impl Lru {
         let mut shard = shard_mu.write().unwrap();
 
         shard.bump(lid);
-=======
-use std::sync::Mutex;
-
-use super::*;
-
-pub struct Cache {
-    shards: Vec<Mutex<Shard>>,
-    cache_capacity: usize,
-    cache_bits: usize,
-}
-
-impl Cache {
-    fn new(cache_capacity: usize, cache_bits: usize) -> Cache {
-        assert!(cache_bits <= 20,
-                "way too many shards. use a smaller number of cache_bits");
-        let size = 2 << cache_bits;
-        Cache {
-            shards: rep_no_copy![Mutex::new(Shard); size],
-            cache_capacity: cache_capacity,
-            cache_bits: cache_bits,
-        }
-    }
-
-    fn get(&self, k: Key) -> Value {
-        let h = hash::hash(&*k, 0) as usize;
-        let idx = h % (2 << self.cache_bits);
-        let ref shard = self.shards[idx].lock().unwrap();
-
-        shard.get(k)
->>>>>>> origin
     }
 }
 
