2c700ebe22b3f0342362ab6a2b6aae0d677c94c9
Tyler Neely <t@jujit.su>
2017-08-07 21:41:29 +0200

    small cleanup

--- src/page/hash.rs
+++ src/page/hash.rs
@@ -1,16 +1,14 @@
 //  Copyright (c) 2017-present, Tyler Neely. All rights reserved.
 //  This source code is licensed under the BSD-style license found in the
-//  LICENSE file in the root directory of this source tree. An additional grant
-//  of patent rights can be found in the PATENTS file in the same directory.
+//  LICENSE file in the root directory of this source tree.
 //
 //  Copyright (c) 2011-present, Facebook, Inc.  All rights reserved.
 //  This source code is licensed under the BSD-style license found in the
-//  LICENSE.rocksdb file in the root directory of this source tree. An additional grant
-//  of patent rights can be found in the PATENTS file in the same directory.
+//  LICENSE.rocksdb file in the root directory of this source tree.
 //
-// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE.leveldb file. See the AUTHORS file for names of contributors.
+//  Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+//  Use of this source code is governed by a BSD-style license that can be
+//  found in the LICENSE.leveldb file.
 
 use std::num::Wrapping;
 
