13b75e54bf75a99d84cb18d162683b2c3cc01627
Tyler Neely <t@jujit.su>
2017-08-23 16:26:36 +0200

    improve arguments to flamerun

--- hack/flamerun.sh
+++ hack/flamerun.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-perf record -F 99 -g "$1"
+perf record -F 99 -g "$@"
 perf script > out.perf
 ./hack/stackcollapse-perf.pl out.perf > out.folded
 ./hack/flamegraph.pl out.folded > flamegraph.svg
