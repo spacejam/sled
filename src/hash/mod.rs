7944f5cdf9a200301504effc23ebd74a84c82f41
Tyler Neely <t@jujit.su>
2017-09-06 14:02:55 +0200

    remove unused hash function, and associated licenses

--- src/hash/mod.rs
+++ src/hash/mod.rs
@@ -1,11 +1,5 @@
-#![allow(dead_code)]
-
-mod keyhash;
 mod crc16;
 mod crc64;
-
-// used for fast lookups
-pub use self::keyhash::hash;
 
 // used for protecting large snapshot files
 pub use self::crc64::crc64;
