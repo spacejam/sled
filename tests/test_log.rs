0a0bbe30d5a31f2082051032d40e6092becf5cbc
Tyler Neely <t@jujit.su>
2017-09-08 22:57:45 +0200

    add truncation tests, fix a small bug in unwrapping an erroneous file read. (#128)

--- tests/test_log.rs
+++ tests/test_log.rs
@@ -5,6 +5,9 @@ extern crate quickcheck;
 extern crate rand;
 extern crate sled;
 
+#[cfg(target_os = "linux")]
+extern crate libc;
+
 use std::fs;
 use std::thread;
 use std::sync::Arc;
@@ -207,18 +210,28 @@ enum Op {
     Read(u64),
     PunchHole(u64),
     Restart,
+    Truncate(u64),
 }
 
 impl Arbitrary for Op {
     fn arbitrary<G: Gen>(g: &mut G) -> Op {
+        static COUNTER: AtomicUsize = ATOMIC_USIZE_INIT;
+        static LEN: AtomicUsize = ATOMIC_USIZE_INIT;
+
+        let mut incr = || {
+            let len = thread_rng().gen_range(0, 2);
+            LEN.fetch_add(len + HEADER_LEN, Ordering::Relaxed);
+            vec![COUNTER.fetch_add(1, Ordering::Relaxed) as u8; len]
+        };
+
         if g.gen_weighted_bool(7) {
             return Op::Restart;
         }
 
-        static COUNTER: AtomicUsize = ATOMIC_USIZE_INIT;
-
-        let mut incr =
-            || vec![COUNTER.fetch_add(1, Ordering::Relaxed) as u8; thread_rng().gen_range(0, 2)];
+        if g.gen_weighted_bool(50) {
+            let len = LEN.load(Ordering::Relaxed);
+            return Op::Truncate(thread_rng().gen_range(0, len as u64));
+        }
 
         let choice = g.gen_range(0, 5);
 
@@ -253,30 +266,8 @@ impl Arbitrary for OpVec {
 
     fn shrink(&self) -> Box<Iterator<Item = OpVec>> {
         let mut smaller = vec![];
-        for i in 0..self.ops.len() {
-            let mut clone = self.clone();
-            clone.ops.remove(i);
-            smaller.push(clone);
-        }
 
-        // reduce write sizes
-        let mut clone = self.clone();
-        let mut lowered = false;
-        for mut op in clone.ops.iter_mut() {
-            match *op {
-                Op::WriteReservation(ref mut buf) |
-                Op::AbortReservation(ref mut buf) |
-                Op::Write(ref mut buf) if buf.len() > 0 => {
-                    buf.clear();
-                }
-                _ => {}
-            }
-        }
-        if lowered {
-            smaller.push(clone);
-        }
-
-        // shrink read offsets
+        // shrink offsets
         let mut clone = self.clone();
         let mut lowered = false;
         for mut op in clone.ops.iter_mut() {
@@ -285,10 +276,25 @@ impl Arbitrary for OpVec {
                     lowered = true;
                     *lid -= 1;
                 }
+                Op::PunchHole(ref mut lid) if *lid > 0 => {
+                    lowered = true;
+                    *lid -= 1;
+                }
+                Op::Truncate(ref mut len) if *len > 0 => {
+                    lowered = true;
+                    *len -= 1;
+                }
                 _ => {}
             }
         }
         if lowered {
+            smaller.push(clone);
+        }
+
+        // drop elements
+        for i in 0..self.ops.len() {
+            let mut clone = self.clone();
+            clone.ops.remove(i);
             smaller.push(clone);
         }
 
@@ -300,8 +306,9 @@ fn prop_log_works(ops: OpVec) -> bool {
     use self::Op::*;
     let config = Config::default();
 
+    let mut tip = 0;
     let mut log = config.log();
-    let mut reference: Vec<(u64, Option<Vec<u8>>)> = vec![];
+    let mut reference: Vec<(u64, Option<Vec<u8>>, usize)> = vec![];
 
     for op in ops.ops.into_iter() {
         match op {
@@ -310,22 +317,10 @@ fn prop_log_works(ops: OpVec) -> bool {
                     continue;
                 }
                 {
-                    let (lid, ref mut expected) = reference[lid as usize];
-                    log.make_stable(lid);
+                    let (lid, ref mut expected, _len) = reference[lid as usize];
+                    log.flush();
                     if expected.take().is_some() {
                         log.punch_hole(lid);
-                    }
-                }
-                if lid > 0 {
-                    // we want to remove this record if the previous record is
-                    // also zeroed, as during reads, zeroed records become
-                    // indistinguishable.
-                    let should_remove = {
-                        let (lid, ref expected) = reference[(lid - 1) as usize];
-                        if expected.is_none() { true } else { false }
-                    };
-                    if should_remove {
-                        reference.remove(lid as usize);
                     }
                 }
             }
@@ -333,30 +328,75 @@ fn prop_log_works(ops: OpVec) -> bool {
                 if reference.len() <= lid as usize {
                     continue;
                 }
-                let (lid, ref expected) = reference[lid as usize];
-                log.make_stable(lid);
-                let read = log.read(lid).unwrap();
-                if expected.is_none() {
-                    assert!(read.is_zeroed());
+                let (lid, ref expected, _len) = reference[lid as usize];
+                log.flush();
+                let read_res = log.read(lid);
+                if expected.is_none() || tip as u64 <= lid {
+                    assert!(read_res.is_err() || !read_res.unwrap().is_flush());
                 } else {
-                    let flush = read.flush();
+                    let flush = read_res.unwrap().flush();
                     assert_eq!(flush, *expected);
                 }
             }
             Write(buf) => {
                 let lid = log.write(buf.clone());
-                reference.push((lid, Some(buf)));
+                let len = buf.len();
+                tip += len + HEADER_LEN;
+                reference.push((lid, Some(buf), len));
             }
             WriteReservation(buf) => {
                 let lid = log.reserve(buf.clone()).complete();
-                reference.push((lid, Some(buf)));
+                let len = buf.len();
+                tip += len + HEADER_LEN;
+                reference.push((lid, Some(buf), len));
             }
             AbortReservation(buf) => {
+                let len = buf.len();
+                tip += len + HEADER_LEN;
                 let lid = log.reserve(buf).abort();
-                reference.push((lid, None));
+                reference.push((lid, None, len));
             }
             Restart => {
                 drop(log);
+                log = config.log();
+            }
+            Truncate(new_len) => {
+                if tip as u64 <= new_len {
+                    // we are testing data loss, not rogue extensions
+                    continue;
+                }
+
+                drop(log);
+
+                #[cfg(target_os = "linux")]
+                {
+                    let path = config.get_path();
+
+                    let mut sz_total = 0;
+                    let mut keep = 0;
+                    for &mut (_, ref mut expected, sz) in &mut reference {
+                        sz_total += sz + HEADER_LEN;
+                        if sz_total <= new_len as usize {
+                            keep += 1;
+                        }
+                    }
+
+                    unsafe {
+                        reference.set_len(keep);
+                    }
+
+                    use std::os::unix::io::AsRawFd;
+
+                    let cached_f = config.cached_file();
+                    let f = cached_f.borrow_mut();
+                    use libc::ftruncate;
+                    let fd = f.as_raw_fd();
+
+                    unsafe {
+                        ftruncate(fd, new_len as i64);
+                    }
+                }
+
                 log = config.log();
             }
         }
@@ -369,7 +409,7 @@ fn quickcheck_log_works() {
     QuickCheck::new()
         .gen(StdGen::new(rand::thread_rng(), 1))
         .tests(50)
-        .max_tests(100)
+        .max_tests(1000)
         .quickcheck(prop_log_works as fn(OpVec) -> bool);
 }
 
@@ -436,6 +476,101 @@ fn test_log_bug_5() {
             Write(vec![44]),
             PunchHole(1),
             Read(0),
+        ],
+    });
+}
+
+#[test]
+fn test_log_bug_6() {
+    // postmortem: test alignment
+    use Op::*;
+    prop_log_works(OpVec {
+        ops: vec![
+            WriteReservation(vec![2]),
+            AbortReservation(vec![3]),
+            AbortReservation(vec![]),
+            Write(vec![5]),
+            AbortReservation(vec![]),
+            PunchHole(3),
+            PunchHole(3),
+            AbortReservation(vec![7]),
+            AbortReservation(vec![9]),
+            WriteReservation(vec![10]),
+            PunchHole(4),
+            WriteReservation(vec![]),
+            PunchHole(3),
+            Truncate(63),
+            PunchHole(3),
+            Read(3),
+        ],
+    });
+}
+
+#[test]
+fn test_log_bug_7() {
+    // postmortem: test alignment
+    use Op::*;
+    prop_log_works(OpVec {
+        ops: vec![
+            WriteReservation(vec![]),
+            WriteReservation(vec![]),
+            AbortReservation(vec![]),
+            Write(vec![12]),
+            AbortReservation(vec![]),
+            Truncate(24),
+            WriteReservation(vec![19]),
+            Write(vec![20]),
+            Read(4),
+        ],
+    });
+}
+
+#[test]
+fn test_log_bug_8() {
+    // postmortem: punch_hole did not protect against the file being too short to
+    // read the size bytes at that offset.
+    use Op::*;
+    prop_log_works(OpVec {
+        ops: vec![Truncate(671), Write(vec![]), Truncate(7), PunchHole(0)],
+    });
+}
+
+#[test]
+fn test_log_bug_9() {
+    // postmortem: test was mishandling truncation, which should test loss, not extension
+    use Op::*;
+    prop_log_works(OpVec {
+        ops: vec![
+            Truncate(99),
+            WriteReservation(vec![234]),
+            Truncate(8),
+            Read(0),
+        ],
+    });
+}
+
+#[test]
+fn test_log_bug_10() {
+    // postmortem: test alignment
+    use Op::*;
+    prop_log_works(OpVec {
+        ops: vec![
+            Write(vec![231]),
+            AbortReservation(vec![232]),
+            WriteReservation(vec![235]),
+            AbortReservation(vec![236]),
+            WriteReservation(vec![]),
+            Write(vec![]),
+            WriteReservation(vec![]),
+            AbortReservation(vec![240]),
+            Truncate(14),
+            AbortReservation(vec![242]),
+            WriteReservation(vec![243]),
+            Write(vec![245]),
+            Write(vec![]),
+            Write(vec![249]),
+            WriteReservation(vec![250]),
+            Read(7),
         ],
     });
 }
