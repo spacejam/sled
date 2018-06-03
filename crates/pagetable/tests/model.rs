035d5cf1c7c2c1be297c8ef052a32eb7868d99c9
Tyler Neely <t@jujit.su>
2018-06-04 00:31:43 +0200

    add pagetable crate

---
+++ crates/pagetable/tests/model.rs
@@ -0,0 +1,73 @@
+#[macro_use]
+extern crate model;
+#[macro_use]
+extern crate proptest;
+extern crate pagetable;
+extern crate crossbeam_epoch as epoch;
+
+use std::collections::HashMap;
+
+use pagetable::PageTable;
+use model::Shared;
+
+#[test]
+fn test_model() {
+    model! {
+        Model => let mut m = HashMap::new(),
+        Implementation => let i = PageTable::default(),
+        Insert((usize, usize))((k, new) in (0usize..4, 0usize..4)) => {
+            if !m.contains_key(&k) {
+                m.insert(k, new);
+                i.insert(k, new).expect("should be able to insert a value");
+            }
+        },
+        Get(usize)(k in 0usize..4) => {
+            let guard = epoch::pin();
+            let expected = m.get(&k);
+            let actual = i.get(k, &guard).map(|s| unsafe { s.deref() });
+            assert_eq!(expected, actual);
+        },
+        Cas((usize, usize, usize))((k, old, new) in (0usize..4, 0usize..4, 0usize..4)) => {
+            let guard = epoch::pin();
+            let expected_current = m.get(&k).cloned();
+            let actual_current = i.get(k, &guard);
+            assert_eq!(expected_current, actual_current.map(|s| unsafe { *s.deref() }));
+            if expected_current.is_none() {
+                continue;
+            }
+
+            let new_v = epoch::Owned::new(new).into_shared(&guard);
+
+            if expected_current == Some(old) {
+                m.insert(k, new);
+                let cas_res = i.cas(k, actual_current.unwrap(), new_v, &guard);
+                assert!(cas_res.is_ok());
+            };
+        }
+    }
+}
+
+#[test]
+#[ignore]
+fn test_linearizability() {
+    linearizable! {
+        Implementation => let i = Shared::new(PageTable::default()),
+        Get(usize)(k in any::<usize>()) -> Option<usize> {
+            let guard = epoch::pin();
+            unsafe {
+                i.get(k, &guard).map(|s| *s.deref())
+            }
+        },
+        Insert((usize, usize))((k, new) in (0usize..4, 0usize..4)) -> Result<(), ()> {
+            i.insert(k, new)
+        },
+        Cas((usize, usize, usize))((k, old, new)
+            in (0usize..4, 0usize..4, 0usize..4))
+            -> Result<(), usize> {
+            let guard = epoch::pin();
+            i.cas(k, epoch::Owned::new(old).into_shared(&guard), epoch::Owned::new(new).into_shared(&guard), &guard)
+                .map(|_| ())
+                .map_err(|s| unsafe { *s.deref() })
+        }
+    }
+}
