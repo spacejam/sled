c1f0397224babde5bb7ef73583d26b52209edf3b
Tyler Neely <t@jujit.su>
2018-09-29 21:16:16 +0200

    Enable a lot more lint denials

--- crates/pagecache/src/lib.rs
+++ crates/pagecache/src/lib.rs
@@ -1,6 +1,12 @@
 //! `pagecache` is a lock-free pagecache and log for building high-performance databases.
 #![deny(missing_docs)]
 #![cfg_attr(test, deny(warnings))]
+#![cfg_attr(test, deny(bad_style))]
+#![cfg_attr(test, deny(future_incompatible))]
+#![cfg_attr(test, deny(nonstandard_style))]
+#![cfg_attr(test, deny(rust_2018_compatibility))]
+#![cfg_attr(test, deny(rust_2018_idioms))]
+#![cfg_attr(test, deny(unused))]
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![cfg_attr(feature = "clippy", allow(inline_always))]
