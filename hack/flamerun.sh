cdf8ee4c28b2400b95823ba6a90ee852465e13a2
Tyler Neely <tyler.neely@ferrous-systems.com>
2018-11-03 08:59:29 +0100

    Merge flamerun scripts together

--- hack/flamerun.sh
+++ hack/flamerun.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -ex
-perf record -F 99 -g "$@"
-perf script > out.perf
-./hack/stackcollapse-perf.pl out.perf > out.folded
-./hack/flamegraph.pl out.folded > flamegraph.svg
-rm perf.data out.perf out.folded
+
+DIR=$(dirname "$0")
+
+case `uname -s` in
+  Darwin)
+    proc_name=${1##*/}
+    echo "expecting process name to be $proc_name"
+    sudo dtrace -c "$*" -o out.stacks -n "profile-997 /execname == \"$proc_name\"/ { @[ustack(100)] = count(); }"
+    ;;
+  Linux)
+    perf record -F 99 -g "$@"
+    perf script > out.stacks
+    ;;
+  *)
+    echo "Only Linux and OSX currently supported. Pull-requests open!"
+    exit 1
+esac
+
+$DIR/stackcollapse.pl out.stacks | $DIR/flamegraph.pl > flamegraph.svg
+sudo rm out.stacks
 
