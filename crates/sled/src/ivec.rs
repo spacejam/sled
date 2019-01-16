3e2e15382973a18eb6a2ccc4185b57030c309332
Tyler Neely <t@jujit.su>
2019-01-16 16:58:57 +0100

    Make IVec 32-bit compatible

--- crates/sled/src/ivec.rs
+++ crates/sled/src/ivec.rs
@@ -8,20 +8,30 @@ const KIND_INLINE: u8 = 0b1000_0000;
 const KIND_OWNED: u8 = 0b0100_0000;
 const KIND_BORROWED: u8 = 0b0010_0000;
 
+#[cfg(target_pointer_width = "64")]
+const CUTOFF: usize = 15;
+#[cfg(target_pointer_width = "64")]
+type Inner = [u8; 16];
+
+#[cfg(target_pointer_width = "32")]
+const CUTOFF: usize = 7;
+#[cfg(target_pointer_width = "32")]
+type Inner = [u8; 8];
+
 #[derive(Default, Ord, Eq, Serialize, Deserialize)]
 pub(crate) struct IVec {
     #[serde(with = "ser")]
-    data: [u8; 16],
+    data: Inner,
 }
 
 impl IVec {
     pub(crate) fn new(v: Vec<u8>) -> IVec {
-        if v.len() <= 15 {
+        if v.len() <= CUTOFF {
             let sz = v.len() as u8;
             let tag = KIND_INLINE | sz;
 
             let mut data = [0u8; 16];
-            data[15] = tag;
+            data[CUTOFF] = tag;
             data[0..v.len()].copy_from_slice(&v[0..v.len()]);
 
             IVec { data }
@@ -32,13 +42,13 @@ impl IVec {
                 unsafe { std::mem::transmute(ptr) };
 
             assert_eq!(
-                data[15], 0,
+                data[CUTOFF], 0,
                 "we incorrectly assumed that we could use \
                  the highest bits in the length field. please \
                  report this bug ASAP!"
             );
 
-            data[15] = KIND_OWNED;
+            data[CUTOFF] = KIND_OWNED;
 
             IVec { data }
         }
@@ -46,12 +56,20 @@ impl IVec {
 
     pub(crate) fn take(&mut self) -> IVec {
         assert_ne!(
-            self.data[15], KIND_BORROWED,
+            self.data[CUTOFF], KIND_BORROWED,
             "take called on Borrowed IVec"
         );
         let ret = IVec { data: self.data };
-        if self.data[15] == KIND_OWNED {
-            self.data[15] = KIND_BORROWED;
+        if self.data[CUTOFF] == KIND_OWNED {
+            self.data[CUTOFF] = KIND_BORROWED;
+        }
+        ret
+    }
+
+    pub(crate) fn borrow(&self) -> IVec {
+        let mut ret = IVec { data: self.data };
+        if ret.data[CUTOFF] == KIND_OWNED {
+            ret.data[CUTOFF] = KIND_BORROWED;
         }
         ret
     }
@@ -67,7 +85,7 @@ impl Deref for IVec {
     type Target = [u8];
 
     fn deref(&self) -> &[u8] {
-        let tag = self.data[15];
+        let tag = self.data[CUTOFF];
         let kind = tag & KIND_MASK;
         match kind {
             k if k == KIND_INLINE => {
@@ -77,7 +95,7 @@ impl Deref for IVec {
             }
             k if (k == KIND_OWNED || k == KIND_BORROWED) => {
                 let mut data: [u8; 16] = self.data;
-                data[15] = 0;
+                data[CUTOFF] = 0;
 
                 unsafe {
                     let ptr: *mut [u8] = std::mem::transmute(data);
@@ -93,18 +111,24 @@ impl Deref for IVec {
 
 impl Clone for IVec {
     fn clone(&self) -> IVec {
-        self.deref().to_vec().into()
+        let ret: IVec = self.deref().to_vec().into();
+        trace!(
+            "cloned IVec pointing to {:?} to one pointing to {:?}",
+            self.deref().as_ptr(),
+            ret.deref().as_ptr(),
+        );
+        ret
     }
 }
 
 impl Drop for IVec {
     fn drop(&mut self) {
-        let tag = self.data[15];
+        let tag = self.data[CUTOFF];
         let kind = tag & KIND_MASK;
         match kind {
             k if k == KIND_OWNED => {
                 let mut data = self.data.clone();
-                data[15] = 0;
+                data[CUTOFF] = 0;
                 unsafe {
                     let ptr: *mut [u8] = std::mem::transmute(data);
                     let b: Box<[u8]> = Box::from_raw(ptr);
@@ -152,7 +176,7 @@ impl fmt::Debug for IVec {
 }
 
 pub(crate) mod ser {
-    use super::{IVec, KIND_BORROWED, KIND_OWNED};
+    use super::{IVec, Inner, CUTOFF, KIND_BORROWED, KIND_OWNED};
 
     use std::ops::Deref;
 
@@ -160,15 +184,15 @@ pub(crate) mod ser {
     use serde::ser::Serializer;
 
     pub(crate) fn serialize<S>(
-        data: &[u8; 16],
+        data: &Inner,
         serializer: S,
     ) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut data: [u8; 16] = *data;
-        if data[15] == KIND_OWNED {
-            data[15] = KIND_BORROWED;
+        let mut data: Inner = *data;
+        if data[CUTOFF] == KIND_OWNED {
+            data[CUTOFF] = KIND_BORROWED;
         }
 
         let iv = IVec { data: data };
@@ -178,14 +202,14 @@ pub(crate) mod ser {
 
     pub(crate) fn deserialize<'de, D>(
         deserializer: D,
-    ) -> Result<[u8; 16], D::Error>
+    ) -> Result<Inner, D::Error>
     where
         D: Deserializer<'de>,
     {
         let v = Vec::<u8>::deserialize(deserializer)?;
         let mut iv: IVec = v.into();
-        let data: [u8; 16] = iv.data;
-        iv.data[15] = KIND_BORROWED;
+        let data: Inner = iv.data;
+        iv.data[CUTOFF] = KIND_BORROWED;
         Ok(data)
     }
 }
