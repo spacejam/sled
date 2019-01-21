5cab0c8fd5b1a864821449816fc35d3c88be61c9
Tyler Neely <t@jujit.su>
2019-01-21 12:27:13 +0100

    Add failing failpoints test

--- tests/tests/test_tree_failpoints.rs
+++ tests/tests/test_tree_failpoints.rs
@@ -883,3 +883,37 @@ fn failpoints_bug_16() {
         false,
     ))
 }
+
+#[test]
+fn failpoints_bug_17() {
+    // postmortem 1:
+    assert!(prop_tree_crashes_nicely(
+        vec![
+            Del(0),
+            Set,
+            Set,
+            Set,
+            Del(3),
+            Id,
+            Id,
+            Set,
+            Id,
+            Id,
+            Del(3),
+            Id,
+            Id,
+            Del(3),
+            Restart,
+            Id,
+            FailPoint("blob blob write"),
+            Id,
+            Restart,
+            Id,
+            Set,
+            Id,
+            Del(3),
+            Set
+        ],
+        false,
+    ))
+}
