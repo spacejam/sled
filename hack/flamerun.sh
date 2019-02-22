205fbe1987d6fabb0238dfb67f39fe4d708e36c8
Tyler Neely <t@jujit.su>
2019-02-22 08:20:14 +0100

    update flamegraph dependency

--- hack/flamerun.sh
+++ hack/flamerun.sh
@@ -8,16 +8,18 @@ case `uname -s` in
     proc_name=${1##*/}
     echo "expecting process name to be $proc_name"
     sudo dtrace -c "$*" -o out.stacks -n "profile-997 /execname == \"$proc_name\"/ { @[ustack(100)] = count(); }"
+    $DIR/stackcollapse.pl out.stacks | $DIR/flamegraph.pl > flamegraph.svg
+    sudo rm out.stacks
     ;;
   Linux)
     perf record -F 99 -g "$@"
     perf script > out.stacks
+    $DIR/stackcollapse-perf.pl out.stacks | $DIR/flamegraph.pl > flamegraph.svg
+    rm out.stacks
     ;;
   *)
     echo "Only Linux and OSX currently supported. Pull-requests open!"
     exit 1
 esac
 
-$DIR/stackcollapse.pl out.stacks | $DIR/flamegraph.pl > flamegraph.svg
-sudo rm out.stacks
 
