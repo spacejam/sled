c086fd279e548d4bfc7a390b8f9cb1a60426e4bc
Tyler Neely <t@jujit.su>
2019-03-16 16:45:04 +0100

    Throw in an explicit mutex unlock for Drop in IoBufs so TSAN can chill out about destruction races

--- crates/pagecache/src/iobuf.rs
+++ crates/pagecache/src/iobuf.rs
@@ -1254,6 +1254,14 @@ impl IoBufs {
 
 impl Drop for IoBufs {
     fn drop(&mut self) {
+        {
+            // this is a hack to let TSAN know
+            // that it's safe to destroy our inner
+            // members, since it doesn't pick up on
+            // the implicit barriers used elsewhere.
+            let _ = self.0.buf_mu.lock().unwrap();
+        }
+
         // don't do any more IO if we're simulating a crash
         #[cfg(feature = "failpoints")]
         {
