c5bda414d47f149458f8d0a65b857ff3b95b78b2
Tyler Neely <t@jujit.su>
2019-06-12 13:15:04 +0200

    Return None from cas_page when the page was already freed

--- crates/pagecache/src/pagecache.rs
+++ crates/pagecache/src/pagecache.rs
@@ -1105,7 +1105,7 @@ where
                             CacheEntry::MergedResident(m, ..) => {
                                 Update::Compact(m)
                             }
-                            CacheEntry::Free(..) => Update::Free,
+                            CacheEntry::Free(..) => return Ok(Err(None)),
                             CacheEntry::Counter(counter, ..) => {
                                 Update::Counter(counter)
                             }
