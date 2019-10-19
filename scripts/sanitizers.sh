58640803aba5ec6f88bafe266367daaa603a3968
Tyler Neely <t@jujit.su>
2019-10-19 14:23:39 +0200

    try using relative TSAN suppression file

--- scripts/sanitizers.sh
+++ scripts/sanitizers.sh
@@ -23,7 +23,7 @@ unset ASAN_OPTIONS
 echo "tsan"
 cargo clean
 export RUSTFLAGS="-Z sanitizer=thread"
-export TSAN_OPTIONS=suppressions=$(Agent.BuildDirectory)/s/tsan_suppressions.txt
+export TSAN_OPTIONS=suppressions=../../tsan_suppressions.txt
 sudo rm -rf default.sled
 cargo +nightly run --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu -- --duration=30
 cargo +nightly run --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu -- --duration=6
