929fbe59518b5aa570c7e9290e7f4a3824fdb962
Tyler Neely <t@jujit.su>
2019-12-09 13:53:25 +0100

    Remove race condition in new threadpool style

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -22,8 +22,41 @@ struct Pool {
 static POOL: Lazy<Pool, fn() -> Pool> = Lazy::new(init_pool);
 
 fn init_pool() -> Pool {
+    maybe_spawn_new_thread();
     let (sender, receiver) = unbounded();
     Pool { sender, receiver }
+}
+
+fn perform_work() {
+    let wait_limit = Duration::from_secs(1);
+
+    loop {
+        debug_delay();
+        STANDBY_THREAD_COUNT.fetch_add(1, SeqCst);
+
+        debug_delay();
+        let task_res = POOL.receiver.recv_timeout(wait_limit);
+
+        debug_delay();
+        if STANDBY_THREAD_COUNT.fetch_sub(1, SeqCst) < 2 {
+            maybe_spawn_new_thread();
+        }
+
+        if let Ok(task) = task_res {
+            (task)();
+        }
+
+        debug_delay();
+        while let Ok(task) = POOL.receiver.try_recv() {
+            (task)();
+            debug_delay();
+        }
+
+        debug_delay();
+        if STANDBY_THREAD_COUNT.load(SeqCst) > 2 {
+            return;
+        }
+    }
 }
 
 // Create up to MAX_THREADS dynamic blocking task worker threads.
@@ -38,37 +71,13 @@ fn maybe_spawn_new_thread() {
         return;
     }
 
-    let spawn_res =
-        thread::Builder::new().name("sled-io".to_string()).spawn(|| {
-            let wait_limit = Duration::from_secs(1);
-
+    let spawn_res = thread::Builder::new()
+        .name("sled-io-dynamic".to_string())
+        .spawn(|| {
             debug_delay();
             TOTAL_THREAD_COUNT.fetch_add(1, SeqCst);
-            loop {
-                debug_delay();
-                STANDBY_THREAD_COUNT.fetch_add(1, SeqCst);
-
-                debug_delay();
-                let task = POOL.receiver.recv_timeout(wait_limit);
-
-                debug_delay();
-                if STANDBY_THREAD_COUNT.fetch_sub(1, SeqCst) == 1 {
-                    maybe_spawn_new_thread();
-                }
-
-                debug_delay();
-                if let Ok(task) = task {
-                    (task)();
-                } else if STANDBY_THREAD_COUNT.compare_and_swap(0, 1, SeqCst)
-                    == 0
-                {
-                    // avoid bottoming out the standby pool
-                    continue;
-                } else {
-                    TOTAL_THREAD_COUNT.fetch_sub(1, SeqCst);
-                    return;
-                }
-            }
+            perform_work();
+            TOTAL_THREAD_COUNT.fetch_sub(1, SeqCst);
         });
 
     if let Err(e) = spawn_res {
