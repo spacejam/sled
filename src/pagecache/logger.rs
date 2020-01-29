ed4356e716fe47b9e7e0d2a35b3e48ccd87a681c
Tyler Neely <t@jujit.su>
2020-01-29 15:34:51 +0100

    Update checksumming logic

--- src/pagecache/logger.rs
+++ src/pagecache/logger.rs
@@ -681,20 +681,18 @@ pub(crate) fn read_message(
     }
 
     // perform crc check on everything that isn't Corrupted
-    let mut buf = Vec::with_capacity(usize::try_from(header.len).unwrap());
-    #[allow(unsafe_code)]
-    unsafe {
-        buf.set_len(usize::try_from(header.len).unwrap());
-    }
+    let mut buf = vec![0; usize::try_from(header.len).unwrap()];
     pread_exact(file, &mut buf, lid + message_offset as LogOffset)?;
 
     // calculate the CRC32, calculating the hash on the
     // header afterwards
     let mut hasher = crc32fast::Hasher::new();
     hasher.update(&buf);
-    hasher.update(msg_header_buf.as_ref());
+    hasher.update(
+        msg_header_buf[std::mem::size_of::<u32>()..message_offset].as_ref(),
+    );
 
-    let crc32 = hasher.finalize();
+    let crc32 = hasher.finalize() ^ 0xFFFF_FFFF;
 
     if crc32 != header.crc32 {
         trace!("read a message with a bad checksum with header {:?}", header);
