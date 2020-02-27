4c52ba66cf32529b962f7e1231d9e48633479f24
Tyler Neely <t@jujit.su>
2020-02-27 08:15:39 +0100

    Drop sanitizer runs from 30 seconds to 10

--- scripts/sanitizers.sh
+++ scripts/sanitizers.sh
@@ -12,7 +12,7 @@ export RUSTFLAGS="-Z sanitizer=address"
 export ASAN_OPTIONS="detect_odr_violation=0"
 cargo +nightly build --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu
 sudo rm -rf default.sled
-sudo target/x86_64-unknown-linux-gnu/debug/stress2 --duration=30
+sudo target/x86_64-unknown-linux-gnu/debug/stress2 --duration=10
 sudo target/x86_64-unknown-linux-gnu/debug/stress2 --duration=6
 unset ASAN_OPTIONS
 
@@ -21,7 +21,7 @@ cargo clean
 export RUSTFLAGS="-Z sanitizer=leak"
 cargo +nightly build --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu
 sudo rm -rf default.sled
-sudo target/x86_64-unknown-linux-gnu/debug/stress2 --duration=30
+sudo target/x86_64-unknown-linux-gnu/debug/stress2 --duration=10
 sudo target/x86_64-unknown-linux-gnu/debug/stress2 --duration=6
 
 echo "tsan"
@@ -29,7 +29,7 @@ cargo clean
 export RUSTFLAGS="-Z sanitizer=thread"
 export TSAN_OPTIONS=suppressions=../../tsan_suppressions.txt
 sudo rm -rf default.sled
-cargo +nightly run --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu -- --duration=30
+cargo +nightly run --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu -- --duration=10
 cargo +nightly run --features=lock_free_delays,no_jemalloc --target x86_64-unknown-linux-gnu -- --duration=6
 unset RUSTFLAGS
 unset TSAN_OPTIONS
