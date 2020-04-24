9c52f3b4b9657bf8b2e3ea5df16c1e2c49a2b4f6
Tyler Neely <t@jujit.su>
2020-04-24 14:02:55 +0200

    Update docs for the Subscriber struct

--- src/subscription.rs
+++ src/subscription.rs
@@ -43,6 +43,42 @@ type Senders =
     HashMap<usize, (Option<Waker>, SyncSender<OneShot<Option<Event>>>)>;
 
 /// A subscriber listening on a specified prefix
+///
+/// `Subscriber` implements both `Iterator<Item = Event>`
+/// and `Future<Output=Option<Event>>`
+///
+/// # Examples
+///
+/// Synchronous, blocking subscription:
+/// ```
+/// use sled::{Config, Event};
+/// let config = Config::new().temporary(true);
+///
+/// let tree = config.open().unwrap();
+///
+/// // watch all events by subscribing to the empty prefix
+/// let mut subscription = tree.watch_prefix(vec![]);
+///
+/// let tree_2 = tree.clone();
+/// let thread = std::thread::spawn(move || {
+///     tree.insert(vec![0], vec![1]).unwrap();
+/// });
+///
+/// // `Subscription` implements `Iterator<Item=Event>`
+/// for event in subscription.take(1) {
+///     match event {
+///         Event::Insert{ key, value } => assert_eq!(key.as_ref(), &[0]),
+///         Event::Remove {key } => {}
+///     }
+/// }
+///
+/// thread.join().unwrap();
+/// ```
+/// Aynchronous, non-blocking subscription:
+///
+/// `Subscription` implements `Future<Output=Option<Event>>`.
+///
+/// `while let Some(event) = subscription.await { /* use it */ }`
 pub struct Subscriber {
     id: usize,
     rx: Receiver<OneShot<Option<Event>>>,
