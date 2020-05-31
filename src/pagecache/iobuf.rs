85379be39d81e2bea8b26ec58081eaff5d8f3884
Tyler Neely <t@jujit.su>
2020-05-31 18:40:48 +0200

    Add more comments to the stabilization code to illuminate some constraints for correctness

--- src/pagecache/iobuf.rs
+++ src/pagecache/iobuf.rs
@@ -212,10 +212,10 @@ impl StabilityIntervals {
     }
 
     pub(crate) fn mark_batch(&mut self, interval: (Lsn, Lsn)) {
+        assert!(interval.0 > self.stable_lsn);
         self.batches.push(interval);
         // reverse sort
         self.batches.sort_unstable_by(|a, b| b.cmp(a));
-        assert!(interval.0 > self.stable_lsn);
     }
 
     fn mark_fsync(&mut self, interval: (Lsn, Lsn)) -> Option<Lsn> {
@@ -268,6 +268,11 @@ impl StabilityIntervals {
 
         let mut batch_stable_lsn = None;
 
+        // batches must be atomically recoverable, which
+        // means that we should wait until the entire
+        // batch has been stabilized before any parts
+        // of the batch are allowed to be reused
+        // due to having marked them as stable.
         while let Some(&(low, high)) = self.batches.last() {
             assert!(
                 low < high,
@@ -275,7 +280,11 @@ impl StabilityIntervals {
                 low,
                 high
             );
+
             if high <= self.stable_lsn {
+                // the entire batch has been written to disk
+                // and fsynced, so we can propagate its stability
+                // through the `batch_stable_lsn` variable.
                 if let Some(bsl) = batch_stable_lsn {
                     assert!(bsl < high);
                 }
@@ -283,6 +292,10 @@ impl StabilityIntervals {
                 self.batches.pop().unwrap();
             } else {
                 if low <= self.stable_lsn {
+                    // the batch has not been fully written
+                    // to disk, but we can communicate that
+                    // the region before the batch has
+                    // stabilized.
                     batch_stable_lsn = Some(low - 1);
                 }
                 break;
