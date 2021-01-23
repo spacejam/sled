b6f233fac1bf01c768c00b4dd2fcebdeb3d1a715
Tyler Neely <t@jujit.su>
2021-01-23 19:38:46 +0100

    perform 2 computed node optimizations for avoiding lots of work when merging overlays or receiving merged siblings, causing write throughput on empty value sequential key workloads to be very high

--- src/node.rs
+++ src/node.rs
@@ -712,6 +712,42 @@ impl Node {
         if self.overlay.is_empty() {
             return self.inner.clone();
         };
+
+        // if this is a node that has a fixed key stride
+        // and empty values, we may be able to skip
+        // the normal merge process by performing a
+        // header-only update
+        let can_seamlessly_absorb = self.fixed_key_stride.is_some()
+            && self.fixed_value_length() == Some(0)
+            && {
+                let mut prev = self.inner.index_key(self.inner.children() - 1);
+                let stride: u16 = self.fixed_key_stride.unwrap().get();
+                let mut length_and_stride_matches = true;
+                for (k, v) in &self.overlay {
+                    length_and_stride_matches &=
+                        v.is_some() && v.as_ref().unwrap().is_empty();
+                    length_and_stride_matches &= KeyRef::Slice(&*k) > prev
+                        && is_linear(&prev, &KeyRef::Slice(&*k), stride);
+
+                    prev = KeyRef::Slice(&*k);
+
+                    if !length_and_stride_matches {
+                        break;
+                    }
+                }
+                length_and_stride_matches
+            };
+
+        if can_seamlessly_absorb {
+            let mut ret = self.inner.deref().clone();
+            ret.children = self
+                .inner
+                .children
+                .checked_add(u32::try_from(self.overlay.len()).unwrap())
+                .unwrap();
+            return Arc::new(ret);
+        }
+
         let mut items =
             Vec::with_capacity(self.inner.children() + self.overlay.len());
 
@@ -2037,6 +2073,28 @@ impl Inner {
         assert_eq!(self.is_index, other.is_index);
         assert!(!self.merging);
         assert!(self.merging_child.is_none());
+        assert!(other.merging_child.is_none());
+
+        // we can shortcut the normal merge process
+        // when the right sibling can be merged into
+        // our node without considering keys or values
+        let can_seamlessly_absorb = self.fixed_key_stride.is_some()
+            && self.fixed_key_stride == other.fixed_key_stride
+            && self.fixed_value_length() == Some(0)
+            && other.fixed_value_length() == Some(0)
+            && self.fixed_key_length == other.fixed_key_length;
+
+        if can_seamlessly_absorb {
+            let mut ret = self.clone();
+            if let Some(other_hi) = other.hi() {
+                ret.hi_mut().unwrap().copy_from_slice(other_hi);
+            }
+            ret.children = self.children.checked_add(other.children).unwrap();
+            ret.next = other.next;
+            ret.rewrite_generations =
+                self.rewrite_generations.max(other.rewrite_generations);
+            return ret;
+        }
 
         let prefix_len = if let Some(right_hi) = other.hi() {
             #[cfg(test)]
