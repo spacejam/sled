c405b938faa997a3b16170fc1181bdcb1302609d
Tyler Neely <t@jujit.su>
2021-01-25 14:38:46 +0100

    Avoid u64's in sys_limits module

--- src/sys_limits.rs
+++ src/sys_limits.rs
@@ -5,6 +5,8 @@ use std::io;
 #[cfg(any(target_os = "linux"))]
 use {std::fs::File, std::io::Read};
 
+use std::convert::TryFrom;
+
 /// See the Kernel's documentation for more information about this subsystem,
 /// found at:  [Documentation/cgroup-v1/memory.txt](https://www.kernel.org/doc/Documentation/cgroup-v1/memory.txt)
 ///
@@ -12,13 +14,13 @@ use {std::fs::File, std::io::Read};
 /// 0x7FFFFFFFFFFFF000 (2^63-1 rounded down to 4k which is a common page size).
 /// So we know we are not running in a memory restricted environment.
 #[cfg(target_os = "linux")]
-fn get_cgroup_memory_limit() -> io::Result<u64> {
+fn get_cgroup_memory_limit() -> io::Result<usize> {
     File::open("/sys/fs/cgroup/memory/memory.limit_in_bytes")
-        .and_then(read_u64_from)
+        .and_then(read_usize_from)
 }
 
 #[cfg(target_os = "linux")]
-fn read_u64_from(mut file: File) -> io::Result<u64> {
+fn read_usize_from(mut file: File) -> io::Result<usize> {
     let mut s = String::new();
     file.read_to_string(&mut s).and_then(|_| {
         s.trim()
@@ -44,9 +46,7 @@ fn get_rlimit_as() -> io::Result<libc::rlimit> {
 }
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
-pub fn get_available_memory() -> io::Result<u64> {
-    use std::convert::TryFrom;
-
+pub fn get_available_memory() -> io::Result<usize> {
     let pages = unsafe { libc::sysconf(libc::_SC_PHYS_PAGES) };
     if pages == -1 {
         return Err(io::Error::last_os_error());
@@ -57,14 +57,11 @@ pub fn get_available_memory() -> io::Result<u64> {
         return Err(io::Error::last_os_error());
     }
 
-    Ok(u64::try_from(pages).unwrap() * u64::try_from(page_size).unwrap())
+    Ok(usize::try_from(pages).unwrap() * usize::try_from(page_size).unwrap())
 }
 
-pub fn get_memory_limit() -> u64 {
-    // Maximum addressable memory space limit in u64
-    static MAX_USIZE: u64 = usize::max_value() as u64;
-
-    let mut max: u64 = 0;
+pub fn get_memory_limit() -> Option<usize> {
+    let mut max: usize = 0;
 
     #[cfg(target_os = "linux")]
     {
@@ -78,14 +75,14 @@ pub fn get_memory_limit() -> u64 {
         // running in a memory restricted environment.
         // src: https://github.com/dotnet/coreclr/blob/master/src/pal/src/misc/cgroup.cpp#L385-L428
         if max > 0x7FFF_FFFF_0000_0000 {
-            return 0;
+            return None;
         }
     }
 
     #[cfg(any(target_os = "linux", target_os = "macos"))]
     {
         if let Ok(rlim) = get_rlimit_as() {
-            let rlim_cur = Into::<u64>::into(rlim.rlim_cur);
+            let rlim_cur = usize::try_from(rlim.rlim_cur).unwrap();
             if max == 0 || rlim_cur < max {
                 max = rlim_cur;
             }
@@ -96,13 +93,6 @@ pub fn get_memory_limit() -> u64 {
                 max = available;
             }
         }
-    }
-
-    if max > MAX_USIZE {
-        // It is observed in practice when the memory is unrestricted, Linux
-        // control group returns a physical limit that is bigger than
-        // the address space
-        max = MAX_USIZE;
     }
 
     #[cfg(miri)]
@@ -116,5 +106,5 @@ pub fn get_memory_limit() -> u64 {
         max /= 40;
     }
 
-    max
+    Some(max)
 }
