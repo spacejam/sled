01bda31c712e213deb45a1dbaf7d37cb4e35cf05
Tyler Neely <t@jujit.su>
2021-01-26 23:47:31 +0100

    Fix cross compilation

--- src/sys_limits.rs
+++ src/sys_limits.rs
@@ -79,10 +79,11 @@ pub fn get_memory_limit() -> Option<usize> {
         }
     }
 
+    #[allow(clippy::useless_conversion)]
     #[cfg(any(target_os = "linux", target_os = "macos"))]
     {
         if let Ok(rlim) = get_rlimit_as() {
-            let rlim_cur = rlim.rlim_cur;
+            let rlim_cur = u64::try_from(rlim.rlim_cur).unwrap();
             if max == 0 || rlim_cur < max {
                 max = rlim_cur;
             }
